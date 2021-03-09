6708f25e1dc6afcdb417613937478a483dad9027
Peter Levinsky <pele@easj.dk>
2021-03-09 09:05:16 +0100

    Add project files.

---
+++ BreweryConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace BreweryConsoleApp
+{
+    class Program
+    {
+        public static TraceSource TRACE;
+        static void Main(string[] args)
+        {
+            TRACE = new TraceSource("Brewery");
+            TRACE.Listeners.Add(
+                new TextWriterTraceListener(
+                    new StreamWriter(@"c:\Logfiler\Brewery.txt")));
+            TRACE.Listeners.Add(new ConsoleTraceListener());
+
+
+            TRACE.Switch = new SourceSwitch("Brewery", "all");
+
+            BreweryWorker worker = new BreweryWorker();
+            worker.Start();
+
+            Console.WriteLine("Simulation finished - look in logfile");
+            Console.ReadLine();
+        }
+    }
+}
